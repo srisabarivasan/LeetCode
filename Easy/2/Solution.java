2179a1d681daefdb8c46331f55173f025b106ad6
Srisabarivasan <plsrisabarivasan@gmail.com>
2025-08-28 22:49:48 +0530

    Update Solution.java

--- Easy/2/Solution.java
+++ Easy/2/Solution.java
@@ -1,19 +1,24 @@
-class Solution
+class Solution 
 {
-    public int[] twoSum(int[] nums, int target) 
+    public boolean isAnagram(String s, String t) 
     {
-       int[] ans = new int[2]; 
-       for (int i=0; i<nums.length; i++) 
+      if(s.length() != t.length())
+      {
+        return false;
+      }
+      int[] freq = new int[26];
+      for(int i=0; i<s.length(); i++)
+      {
+        freq[s.charAt(i)-'a']++;
+        freq[t.charAt(i)-'a']--;
+      }
+      for (int c : freq) 
+      {
+       if(c != 0)
        {
-            for(int j=i+1; j<nums.length; j++) 
-            { 
-                if(nums[i]+nums[j]==target) 
-                {
-                   ans[0]=i;
-                   ans[1]=j;
-                }
-            }
+        return false;
        }
-       return ans;
+      }
+      return true;
     }
-}
+} 
